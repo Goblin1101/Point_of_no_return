39be8044288e768de37fb5fef497c62dd181b081
Goblin <trollface831@gmail.com>
2023-11-14 15:44:25 +0700

    попытка всё закинуть на хаб

---
+++ Source/Testing/Public/CPP_CameraRotationControll.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CPP_CameraRotationControll.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTING_API ACPP_CameraRotationControll : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
